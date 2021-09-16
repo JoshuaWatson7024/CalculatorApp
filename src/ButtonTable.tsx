4b3ef7dc73debaedd1447d141962bba0ace5c88e
Josh Watson <joshua@udel.edu>
2021-09-16 19:51:58 -0400

    one fxn in buttonTable

--- src/ButtonTable.tsx
+++ src/ButtonTable.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import { useState } from "react"
 
-function ButtonTable(): JSX.Element{
-    
+export const SwapButton = ({change}: {change: (text: string) => void}): JSX.Element => {
+  const [active, setActive] = useState<boolean>(false);
+
+  const toggle = () => {
+    if (active) {
+      change("FIRST PROMPT");
+    } else {
+      change("SECOND PROMPT");
+    }
+    setActive(!active);
+  }
+
+  return <button onClick={() => toggle()}>Swap Prompt</button>
 }
-
