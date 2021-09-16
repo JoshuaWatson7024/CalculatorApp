6ab275eacfb905650b2fdb4bd4298124be16a911
Josh Watson <joshua@udel.edu>
2021-09-16 19:47:10 -0400

    Architechture set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './ButtonTable';
+import './Display';
 
-function App() {
+function App(): JSX.Element {
+  const [display, setDisplay] = useState<string>("Do Something!");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display text={display}></display>
+      <ButtonTable change={setDisplay}></ButtonTable>
+
     </div>
   );
 }
