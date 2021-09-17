841e33b128acff125cbc46ba4cdfe4540d71adbd
Josh Watson <joshua@udel.edu>
2021-09-16 20:50:08 -0400

    1-9

--- src/ButtonTable.tsx
+++ src/ButtonTable.tsx
@@ -5,7 +5,35 @@ export const ButtonTable = ({change, disp}: {change: (text: string) => void; dis
     <table>
         <tr>
             <td>
-                <button onClick={() => change(disp + "7")}>7</button>
+                <button className="numBut" onClick={() => change(disp + "7")}>7</button>
+            </td>
+            <td>
+                <button className="numBut" onClick={() => change(disp + "8")}>8</button>
+            </td>
+            <td>
+                <button className="numBut" onClick={() => change(disp + "9")}>9</button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button className="numBut" onClick={() => change(disp + "4")}>4</button>
+            </td>
+            <td>
+                <button className="numBut" onClick={() => change(disp + "5")}>5</button>
+            </td>
+            <td>
+                <button className="numBut" onClick={() => change(disp + "6")}>6</button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button className="numBut" onClick={() => change(disp + "1")}>1</button>
+            </td>
+            <td>
+                <button className="numBut" onClick={() => change(disp + "2")}>2</button>
+            </td>
+            <td>
+                <button className="numBut" onClick={() => change(disp + "3")}>3</button>
             </td>
         </tr>
     </table>
